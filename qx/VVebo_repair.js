c3d92dfb7cf48751f16014fbf024308119fad957
jiangyiUI <jiangyi.ui@gmail.com>
2023-11-27 16:54:19 +0800

    Update VVebo_repair.js

--- qx/VVebo_repair.js
+++ qx/VVebo_repair.js
@@ -7,6 +7,8 @@
 ^https:\/\/api\.weibo\.cn\/2\/users\/show\? url script-response-body https://raw.githubusercontent.com/jiangyiUI/jiangyi/main/qx/vvebo.js
 ^https:\/\/api\.weibo\.cn\/2\/statuses\/user_timeline\? url script-response-body https://raw.githubusercontent.com/jiangyiUI/jiangyi/main/qx/vvebo.js
 ^https:\/\/api\.weibo\.cn\/2\/profile\/statuses\/tab\? url script-response-body https://raw.githubusercontent.com/jiangyiUI/jiangyi/main/qx/vvebo.js
+
+
 [mitm]
 
 hostname = api.weibo.cn
