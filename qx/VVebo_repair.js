438ed35d47f4a486bfff4711743643209469d837
jiangyiUI <jiangyi.ui@gmail.com>
2023-11-27 16:56:57 +0800

    Update VVebo_repair.js

--- qx/VVebo_repair.js
+++ qx/VVebo_repair.js
@@ -4,8 +4,8 @@
 
 [rewrite_local]
 
-^https:\/\/api\.weibo\.cn\/2\/users\/show\? url script-response-body https://raw.githubusercontent.com/jiangyiUI/jiangyi/main/qx/vvebo.js
-^https:\/\/api\.weibo\.cn\/2\/statuses\/user_timeline\? url script-response-body https://raw.githubusercontent.com/jiangyiUI/jiangyi/main/qx/vvebo.js
+^https:\/\/api\.weibo\.cn\/2\/users\/show\? url script-request-body https://raw.githubusercontent.com/jiangyiUI/jiangyi/main/qx/vvebo.js
+^https:\/\/api\.weibo\.cn\/2\/statuses\/user_timeline\? url script-request-body https://raw.githubusercontent.com/jiangyiUI/jiangyi/main/qx/vvebo.js
 ^https:\/\/api\.weibo\.cn\/2\/profile\/statuses\/tab\? url script-response-body https://raw.githubusercontent.com/jiangyiUI/jiangyi/main/qx/vvebo.js
 
 
